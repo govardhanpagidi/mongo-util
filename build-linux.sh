94639a361233742e2503d6712c087599836ed6c1
govardhan <inboxgovardhan@gmail.com>
2022-04-25 13:53:50 -0500

    build changes

--- build-linux.sh
+++ build-linux.sh
@@ -24,11 +24,20 @@ then
 fi
 
 env GOOS=${os} GOARCH=${arch} go build -o mongo_util_${os}_${arch}
-
+if [ $? -ne 0 ];
+then
+  echo "Build is failed."
+  exit 1
+fi
 echo "Build is successful."
 
 pwd
 
 ./mongo_util_linux_amd64 ${projectName}
 
+if [ $? -ne 0 ];
+then
+  echo "Job execution failed"
+  exit 1
+fi
 echo "Job is executed"
